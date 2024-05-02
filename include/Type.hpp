4d4366002fa3c7fc5a71bae97b310d580d03cbe5
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-02 20:27:48 +0200

    Create Type.hpp

---
+++ include/Type.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file Type.hpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief 
+ * @date 2024-05-02
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef TYPE_PACS
+#define TYPE_PACS
+
+// Concepts.
+#include <concepts>
+
+// Math.
+#include <complex>
+#include <cmath>
+
+namespace pacs {
+
+    // Numeric type concept for Matrix and Vector.
+
+    /**
+     * @brief Addable types.
+     * 
+     * @tparam T 
+     */
+    template<typename T>
+    concept Addable = requires(T first, T second) {
+        {first += second} -> std::convertible_to<T>;
+        {first -= second} -> std::convertible_to<T>;
+    };
+
+    /**
+     * @brief Multipliable types.
+     * 
+     * @tparam T 
+     */
+    template<typename T>
+    concept Multipliable = requires(T first, T second) {
+        {first *= second} -> std::convertible_to<T>;
+        {first /= second} -> std::convertible_to<T>;
+    };
+
+    /**
+     * @brief Absolute value supported types.
+     * 
+     * @tparam T 
+     */
+    template<typename T>
+    concept Absolute = requires(T value) {
+        {std::abs(value)} -> std::convertible_to<double>;
+    };
+
+    /**
+     * @brief Numeric type.
+     * 
+     * @tparam T 
+     */
+    template<typename T>
+    concept NumericType = Addable<T> && Multipliable<T> && Absolute<T>;
+
+}
+
+#endif
