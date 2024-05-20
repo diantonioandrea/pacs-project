59369de5fdcfbe203cb640192b3cca98c19cd286
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-20 11:02:19 +0200

    Added mesh reading

--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -126,6 +126,7 @@ namespace pacs {
     // Implemented under src/Builder.cpp
 
     std::vector<Polygon> mesh_diagram(const Polygon &, const std::size_t &, const bool &reflect = false);
+    std::vector<Polygon> mesh_diagram(const std::string &);
     std::vector<Polygon> mesh_refine(const Mesh &, const std::vector<std::size_t> &);
 
     std::vector<Point> mesh_nodes(const std::vector<Polygon> &);
--- src/Builder.cpp
+++ src/Builder.cpp
@@ -10,6 +10,12 @@
 
 #include <Mesh.hpp>
 
+// IO handling.
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
 // Small edges tolerance.
 #ifndef COLLAPSE_TOLERANCE
 #define COLLAPSE_TOLERANCE 1E-1
@@ -148,6 +154,53 @@ namespace pacs {
         return diagram;
     }
 
+    /**
+     * @brief Reads a diagram from a file.
+     * 
+     * @param filename 
+     * @return std::vector<Polygon> 
+     */
+    std::vector<Polygon> mesh_diagram(const std::string &filename) {
+        // File loading.
+        std::ifstream file{filename};
+
+        // Diagram.
+        std::vector<Polygon> diagram;
+
+        // Reading.
+        std::string line;
+        
+        while (std::getline(file, line)) {
+
+            // Skip lines starting with '@'
+            if (!line.empty() && line[0] == '@') {
+                continue;
+            }
+
+            // Reading points.
+            std::istringstream lineStream{line};
+
+            std::vector<Point> points;
+            Real x, y;
+
+            while (lineStream >> x >> y) {
+                Point point{x, y};
+                points.emplace_back(point);
+            }
+
+            diagram.emplace_back(points);
+        }
+
+        return diagram;
+    }
+
+    /**
+     * @brief Refines specified elements from a mesh.
+     * 
+     * @param mesh 
+     * @param indices 
+     * @return std::vector<Polygon> 
+     */
     std::vector<Polygon> mesh_refine(const Mesh &mesh, const std::vector<std::size_t> &indices) {
         #ifndef NDEBUG // Integrity check.
         for(std::size_t j = 0; j < indices.size(); ++j)
