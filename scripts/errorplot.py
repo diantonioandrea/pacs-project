7a39caeb259785774b3310fc4454fe677159d513
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-21 19:03:04 +0200

    Better error visualization

--- scripts/errorplot.py
+++ scripts/errorplot.py
@@ -15,6 +15,7 @@ import sys
 elements: list[int] = []
 sizes: list[float] = []
 l2_error: list[float] = []
+dg_error: list[float] = []
 order: int = 0
 
 if len(sys.argv) <= 1:
@@ -22,6 +23,8 @@ if len(sys.argv) <= 1:
     sys.exit(0)
 
 try:
+    if sys.argv[1].split(".")[-1] != "error":
+        raise
 
     file = open(sys.argv[1], "r+")
     lines: list[str] = file.read().split("\n")
@@ -29,6 +32,10 @@ try:
 
 except FileNotFoundError:
     print("File not found.")
+    sys.exit(-1)
+
+except:
+    print("Load a .error file.")
     sys.exit(-1)
 
 # Simple scraping.
@@ -45,8 +52,8 @@ for line in lines:
         elif "L2 Error" in line:
             l2_error.append(float(data[-1]))
 
-        elif "L2 Error" in line:
-            l2_error.append(float(data[-1]))
+        elif "DG Error" in line:
+            dg_error.append(float(data[-1]))
 
         elif "Degree" in line:
             order = int(data[-1])
@@ -55,17 +62,32 @@ for line in lines:
         continue
 
 # Comparisons.
-elements_comparison: list[list[float]] = [None] * 3
-sizes_comparison: list[list[float]] = [None] * 3
 
-elements_comparison[0] = [(value / elements[-1]) ** -(order - 1) * l2_error[-1] for value in elements]
-sizes_comparison[0] = [(value / sizes[0]) ** (order - 1) * l2_error[0] for value in sizes]
+# L2.
+elements_comparison_l2: list[list[float]] = [None] * 3
+sizes_comparison_l2: list[list[float]] = [None] * 3
 
-elements_comparison[1] = [(value / elements[-1]) ** -order * l2_error[-1] for value in elements]
-sizes_comparison[1] = [(value / sizes[0]) ** order * l2_error[0] for value in sizes]
+elements_comparison_l2[0] = [(value / elements[-1]) ** -(order - 1) * l2_error[-1] for value in elements]
+sizes_comparison_l2[0] = [(value / sizes[0]) ** order * l2_error[0] for value in sizes]
 
-elements_comparison[2] = [(value / elements[-1]) ** -(order + 1) * l2_error[-1] for value in elements]
-sizes_comparison[2] = [(value / sizes[0]) ** (order + 1) * l2_error[0] for value in sizes]
+elements_comparison_l2[1] = [(value / elements[-1]) ** -order * l2_error[-1] for value in elements]
+sizes_comparison_l2[1] = [(value / sizes[0]) ** (order + 1) * l2_error[0] for value in sizes]
+
+elements_comparison_l2[2] = [(value / elements[-1]) ** -(order + 1) * l2_error[-1] for value in elements]
+sizes_comparison_l2[2] = [(value / sizes[0]) ** (order + 2) * l2_error[0] for value in sizes]
+
+# DG.
+elements_comparison_dg: list[list[float]] = [None] * 3
+sizes_comparison_dg: list[list[float]] = [None] * 3
+
+elements_comparison_dg[0] = [(value / elements[-1]) ** -(order - 1) * dg_error[-1] for value in elements]
+sizes_comparison_dg[0] = [(value / sizes[0]) ** order * dg_error[0] for value in sizes]
+
+elements_comparison_dg[1] = [(value / elements[-1]) ** -order * dg_error[-1] for value in elements]
+sizes_comparison_dg[1] = [(value / sizes[0]) ** (order + 1) * dg_error[0] for value in sizes]
+
+elements_comparison_dg[2] = [(value / elements[-1]) ** -(order + 1) * dg_error[-1] for value in elements]
+sizes_comparison_dg[2] = [(value / sizes[0]) ** (order + 2) * dg_error[0] for value in sizes]
 
 # Ticks.
 elements_ticks = [elements[0], elements[-1]]
@@ -74,8 +96,11 @@ elements_labels = [str(tick) for tick in elements_ticks]
 sizes_ticks = [sizes[0], sizes[-1]]
 sizes_labels = [f"{tick:1.2f}" for tick in sizes_ticks]
 
-errors_ticks = [l2_error[0], l2_error[-1]]
-errors_labels = [f"{tick:1.2e}" for tick in errors_ticks]
+errors_l2_ticks = [l2_error[0], l2_error[-1]]
+errors_dg_ticks = [dg_error[0], dg_error[-1]]
+
+errors_l2_labels = [f"{tick:1.2e}" for tick in errors_l2_ticks]
+errors_dg_labels = [f"{tick:1.2e}" for tick in errors_dg_ticks]
 
 # Title.
 title: str = lines[0]
@@ -83,77 +108,111 @@ title: str = lines[0]
 if "--p" in sys.argv:
     title += f" Order: {order}."
 
-if "--single" in sys.argv: # Elements only.
+if "--elements" in sys.argv: # Elements only.
     # Plot.
-    fig, axes = plt.subplots()
+    fig, axes = plt.subplots(1, 2)
     fig.suptitle(title)
 
     # Elements.
     if "--p" in sys.argv:
-        axes.plot(elements, elements_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
-        axes.plot(elements, elements_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
-        axes.plot(elements, elements_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
-    axes.plot(elements, l2_error, color="black") # Errors.
+        axes[0].plot(elements, elements_comparison_l2[0], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: -{order - 1}") # Comparison.
+        axes[0].plot(elements, elements_comparison_l2[1], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: -{order}") # Comparison.
+        axes[0].plot(elements, elements_comparison_l2[2], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: -{order + 1}") # Comparison.
+    axes[0].plot(elements, l2_error, color="black", marker="*", linewidth=1.0) # Errors.
 
-    # Loglog scale.
-    axes.set_xscale("log")
-    axes.set_yscale("log")
+    if "--p" in sys.argv:
+        axes[1].plot(elements, elements_comparison_dg[1], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: -{order}") # Comparison.
+        axes[1].plot(elements, elements_comparison_dg[2], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: -{order + 1}") # Comparison.
+        axes[1].plot(elements, elements_comparison_dg[0], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: -{order - 1}") # Comparison.
+    axes[1].plot(elements, dg_error, color="black", marker="*", linewidth=1.0) # Errors.
 
+    # Parameters.
+    for j in range(2):
+
+        # Loglog scale.
+        axes[j].set_xscale("log")
+        axes[j].set_yscale("log")
+
+        # Ticks.
+        axes[j].xaxis.set_minor_formatter(NullFormatter())
+        axes[j].yaxis.set_minor_formatter(NullFormatter())
+
+        # Labels.
+        axes[j].set_xlabel("Elements")
+
+        # Legend.
+        axes[j].legend()
+    
     # Ticks.
-    axes.xaxis.set_minor_formatter(NullFormatter())
-    axes.yaxis.set_minor_formatter(NullFormatter())
-    axes.set_xticks(elements_ticks, labels=elements_labels)
-    axes.set_yticks(errors_ticks, labels=errors_labels)
+    axes[0].set_xticks(elements_ticks, labels=elements_labels)
+    axes[0].set_yticks(errors_l2_ticks, labels=errors_l2_labels)
+    axes[1].set_xticks(elements_ticks, labels=elements_labels)
+    axes[1].set_yticks(errors_dg_ticks, labels=errors_dg_labels)
 
-    # Label.
-    axes.set_xlabel("Elements")
-
-    # Legend.
-    axes.legend()
+    # Titles.
+    axes[0].set_title("L2 Error")
+    axes[1].set_title("DG Error")
 
     plt.show()
     sys.exit(0)
 
 # Plot.
-fig, axes = plt.subplots(1, 2)
+fig, axes = plt.subplots(2, 2)
 fig.suptitle(title)
 
 # Elements.
 if "--p" in sys.argv:
-    axes[0].plot(elements, elements_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
-    axes[0].plot(elements, elements_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
-    axes[0].plot(elements, elements_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
-axes[0].plot(elements, l2_error, color="black") # Errors.
+    axes[0, 0].plot(elements, elements_comparison_l2[0], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: -{order}") # Comparison.
+    axes[0, 0].plot(elements, elements_comparison_l2[1], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: -{order + 1}") # Comparison.
+    axes[0, 0].plot(elements, elements_comparison_l2[2], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: -{order - 1}") # Comparison.
+axes[0, 0].plot(elements, l2_error, color="black", marker="*", linewidth=1.0) # Errors.
+
+if "--p" in sys.argv:
+    axes[1, 0].plot(elements, elements_comparison_dg[0], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: -{order - 1}") # Comparison.
+    axes[1, 0].plot(elements, elements_comparison_dg[1], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: -{order}") # Comparison.
+    axes[1, 0].plot(elements, elements_comparison_dg[2], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: -{order + 1}") # Comparison.
+axes[1, 0].plot(elements, dg_error, color="black", marker="*", linewidth=1.0) # Errors.
 
 # Sizes.
 if "--p" in sys.argv:
-    axes[1].plot(sizes, sizes_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
-    axes[1].plot(sizes, sizes_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
-    axes[1].plot(sizes, sizes_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
-axes[1].plot(sizes, l2_error, color="black") # Errors.
+    axes[0, 1].plot(sizes, sizes_comparison_l2[0], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: {order}") # Comparison.
+    axes[0, 1].plot(sizes, sizes_comparison_l2[1], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: {order + 1}") # Comparison.
+    axes[0, 1].plot(sizes, sizes_comparison_l2[2], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: {order + 2}") # Comparison.
+axes[0, 1].plot(sizes, l2_error, color="black", marker="*", linewidth=1.0) # Errors.
 
-# Loglog scale.
-axes[0].set_xscale("log")
-axes[0].set_yscale("log")
-axes[1].set_xscale("log")
-axes[1].set_yscale("log")
+if "--p" in sys.argv:
+    axes[1, 1].plot(sizes, sizes_comparison_dg[0], linewidth=1.0, alpha=0.5, linestyle=":", color="red", label=f"Order: {order}") # Comparison.
+    axes[1, 1].plot(sizes, sizes_comparison_dg[1], linewidth=1.0, alpha=0.5, linestyle="-", color="red", label=f"Order: {order + 1}") # Comparison.
+    axes[1, 1].plot(sizes, sizes_comparison_dg[2], linewidth=1.0, alpha=0.5, linestyle="--", color="red", label=f"Order: {order + 2}") # Comparison.
+axes[1, 1].plot(sizes, dg_error, color="black", marker="*", linewidth=1.0) # Errors.
 
-# Ticks.
-axes[0].xaxis.set_minor_formatter(NullFormatter())
-axes[1].xaxis.set_minor_formatter(NullFormatter())
-axes[0].yaxis.set_minor_formatter(NullFormatter())
-axes[1].yaxis.set_minor_formatter(NullFormatter())
-axes[0].set_xticks(elements_ticks, labels=elements_labels)
-axes[0].set_yticks(errors_ticks, labels=errors_labels)
-axes[1].set_xticks(sizes_ticks, labels=sizes_labels)
-axes[1].set_yticks(errors_ticks, labels=errors_labels)
+# Parameters.
+for j in range(2):
+    for k in range(2):
 
-# Labels.
-axes[0].set_xlabel("Elements")
-axes[1].set_xlabel("Size")
+        # Loglog scale.
+        axes[j, k].set_xscale("log")
+        axes[j, k].set_yscale("log")
 
-# Legends.
-axes[0].legend()
-axes[1].legend()
+        # Formatter.
+        axes[j, k].xaxis.set_minor_formatter(NullFormatter())
+        axes[j, k].yaxis.set_minor_formatter(NullFormatter())
+
+        # Legend.
+        axes[j, k].legend()
+
+    # Ticks.
+    axes[j, 0].set_xticks(elements_ticks, labels=elements_labels)
+    axes[j, 1].set_xticks(sizes_ticks, labels=sizes_labels)
+    axes[0, j].set_yticks(errors_l2_ticks, labels=errors_l2_labels)
+    axes[1, j].set_yticks(errors_dg_ticks, labels=errors_dg_labels)
+    
+    # Labels.
+    axes[j, 0].set_xlabel("Elements")
+    axes[j, 1].set_xlabel("Size")
+
+    # Titles.
+    axes[0, j].set_title("L2 Error")
+    axes[1, j].set_title("DG Error")
 
 plt.show()
