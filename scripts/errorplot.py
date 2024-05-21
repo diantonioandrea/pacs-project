ea5986fc879953f96ae4ca8eee00755db8e9cdc4
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-21 17:25:32 +0200

    Better error plot

--- scripts/errorplot.py
+++ scripts/errorplot.py
@@ -14,7 +14,8 @@ import sys
 
 elements: list[int] = []
 sizes: list[float] = []
-errors: list[float] = []
+l2_error: list[float] = []
+order: int = 0
 
 if len(sys.argv) <= 1:
     print(f"Usage: {sys.argv[0]} /path/to/file.")
@@ -42,17 +43,29 @@ for line in lines:
             sizes.append(float(data[-1]))
 
         elif "L2 Error" in line:
-            errors.append(float(data[-1]))
+            l2_error.append(float(data[-1]))
+
+        elif "L2 Error" in line:
+            l2_error.append(float(data[-1]))
+
+        elif "Degree" in line:
+            order = int(data[-1])
 
     except ValueError:
         continue
 
 # Comparisons.
-elements_linear = [1.0 / (value / elements[-1]) * errors[-1] for value in elements]
-elements_square_root = [1.0 / ((value / elements[-1]) ** 0.5) * errors[-1] for value in elements]
+elements_comparison: list[list[float]] = [None] * 3
+sizes_comparison: list[list[float]] = [None] * 3
 
-sizes_linear = [value * (errors[0] / sizes[0]) for value in sizes]
-sizes_quadratic = [(value / sizes[0]) ** 2 * errors[0] for value in sizes]
+elements_comparison[0] = [(value / elements[-1]) ** -(order - 1) * l2_error[-1] for value in elements]
+sizes_comparison[0] = [(value / sizes[0]) ** (order - 1) * l2_error[0] for value in sizes]
+
+elements_comparison[1] = [(value / elements[-1]) ** -order * l2_error[-1] for value in elements]
+sizes_comparison[1] = [(value / sizes[0]) ** order * l2_error[0] for value in sizes]
+
+elements_comparison[2] = [(value / elements[-1]) ** -(order + 1) * l2_error[-1] for value in elements]
+sizes_comparison[2] = [(value / sizes[0]) ** (order + 1) * l2_error[0] for value in sizes]
 
 # Ticks.
 elements_ticks = [elements[0], elements[-1]]
@@ -61,18 +74,26 @@ elements_labels = [str(tick) for tick in elements_ticks]
 sizes_ticks = [sizes[0], sizes[-1]]
 sizes_labels = [f"{tick:1.2f}" for tick in sizes_ticks]
 
-errors_ticks = [errors[0], errors[-1]]
+errors_ticks = [l2_error[0], l2_error[-1]]
 errors_labels = [f"{tick:1.2e}" for tick in errors_ticks]
+
+# Title.
+title: str = lines[0]
+
+if "--p" in sys.argv:
+    title += f" Order: {order}."
 
 if "--single" in sys.argv: # Elements only.
     # Plot.
     fig, axes = plt.subplots()
-    fig.suptitle(lines[0])
+    fig.suptitle(title)
 
     # Elements.
-    axes.plot(elements, elements_linear, linewidth=1.0, linestyle=":", color="red", label="Linear") # Linear comparison.
-    axes.plot(elements, elements_square_root, linewidth=1.0, linestyle="--", color="red", label="Square root") # Square root comparison.
-    axes.plot(elements, errors, color="black") # Errors.
+    if "--p" in sys.argv:
+        axes.plot(elements, elements_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
+        axes.plot(elements, elements_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
+        axes.plot(elements, elements_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
+    axes.plot(elements, l2_error, color="black") # Errors.
 
     # Loglog scale.
     axes.set_xscale("log")
@@ -95,17 +116,21 @@ if "--single" in sys.argv: # Elements only.
 
 # Plot.
 fig, axes = plt.subplots(1, 2)
-fig.suptitle(lines[0])
+fig.suptitle(title)
 
 # Elements.
-axes[0].plot(elements, elements_linear, linewidth=1.0, linestyle=":", color="red", label="Linear") # Linear comparison.
-axes[0].plot(elements, elements_square_root, linewidth=1.0, linestyle="--", color="red", label="Square root") # Square root comparison.
-axes[0].plot(elements, errors, color="black") # Errors.
+if "--p" in sys.argv:
+    axes[0].plot(elements, elements_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
+    axes[0].plot(elements, elements_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
+    axes[0].plot(elements, elements_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
+axes[0].plot(elements, l2_error, color="black") # Errors.
 
 # Sizes.
-axes[1].plot(sizes, sizes_linear, linewidth=1.0, linestyle=":", color="red", label="Linear") # Linear comparison.
-axes[1].plot(sizes, sizes_quadratic, linewidth=1.0, linestyle="--", color="red", label="Quadratic") # Quadratic comparison.
-axes[1].plot(sizes, errors, color="black") # Errors.
+if "--p" in sys.argv:
+    axes[1].plot(sizes, sizes_comparison[0], linewidth=1.0, linestyle=":", color="red", label=f"Order: {order - 1}") # Comparison.
+    axes[1].plot(sizes, sizes_comparison[1], linewidth=1.0, linestyle="-", color="red", label=f"Order: {order}") # Comparison.
+    axes[1].plot(sizes, sizes_comparison[2], linewidth=1.0, linestyle="--", color="red", label=f"Order: {order + 1}") # Comparison.
+axes[1].plot(sizes, l2_error, color="black") # Errors.
 
 # Loglog scale.
 axes[0].set_xscale("log")
