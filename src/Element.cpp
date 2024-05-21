d13b320642c715ad92a90afb6baa896eabf47140
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-21 22:56:59 +0200

    Update Element.cpp

--- src/Element.cpp
+++ src/Element.cpp
@@ -23,7 +23,7 @@ namespace pacs {
      * @param nodes 
      * @param edges 
      */
-    Element::Element(const std::vector<std::size_t> &nodes, const std::vector<std::size_t> &edges): nodes{nodes}, edges{edges}, degree{3} {}
+    Element::Element(const std::vector<std::size_t> &nodes, const std::vector<std::size_t> &edges): nodes{nodes}, edges{edges}, degree{1} {}
 
     /**
      * @brief Construct a new Element:: Element object
