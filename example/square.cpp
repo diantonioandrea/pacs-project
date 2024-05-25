628b937e0981525ecbe312b3bb97bf7d81c6f505
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-05-25 23:16:22 +0200

    Update square.cpp

--- example/square.cpp
+++ example/square.cpp
@@ -69,6 +69,7 @@ int main() {
         pacs::Vector<pacs::Real> forcing = pacs::forcing(mesh, source, dirichlet);
         
         // Linear system solution.
+        pacs::Vector<pacs::Real> numerical = laplacian.solve<pacs::GMRES>(forcing);
 
         // Errors.
         pacs::Error error{mesh, {mass, dg_laplacian}, numerical, exact};
