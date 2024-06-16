a42e71ad9a60a8eda5f0d1db4146da7c5026c230
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-06-16 22:30:59 +0200

    Update lshape_domain.cpp

--- domains/lshape_domain.cpp
+++ domains/lshape_domain.cpp
@@ -1,1 +1,41 @@
+/**
+ * @file lshape_domain.cpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief 
+ * @date 2024-06-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <Geometry.hpp>
+
 #include <cmath>
+#include <string>
+
+int main() {
+
+    // Domain.
+    pacs::Point a{0.0, 0.0};
+    pacs::Point b{0.5, 0.0};
+    pacs::Point c{0.5, 0.5};
+    pacs::Point d{1.0, 0.5};
+    pacs::Point e{1.0, 1.0};
+    pacs::Point f{0.0, 1.0};
+
+    pacs::Polygon domain{{a, b, c, d, e, f}};
+
+    // Meshes.
+    for(std::size_t j = 0; j < 4; ++j) {
+        
+        // Elements.
+        std::size_t elements = 100 * std::pow(2, j);
+
+        // Mesh.
+        pacs::Mesh mesh{domain, pacs::mesh_diagram(domain, elements, false, true)};
+
+        // Output.
+        mesh.write("output/lshape_" + std::to_string(elements) + ".poly");
+    }
+
+}
