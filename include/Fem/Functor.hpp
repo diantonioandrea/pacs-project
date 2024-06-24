9b55fc2ad628229f9064d87598202af2e6f8e58a
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-06-24 21:43:33 +0200

    Update Functor.hpp

--- include/Fem/Functor.hpp
+++ include/Fem/Functor.hpp
@@ -17,13 +17,6 @@
 namespace pacs {
 
     /**
-     * @brief Zero function.
-     * 
-     * @return Real 
-     */
-    inline Real zero(const Real &, const Real &) { return 0.0; }
-
-    /**
      * @brief Functor class.
      * 
      */
