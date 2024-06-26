c42af1f917539f9699a84d09bf0f4ab60bb4d62e
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-06-26 21:14:06 +0200

    Added a new snippet

---
+++ snippets/h_refine.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file h_refine.cpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief 
+ * @date 2024-06-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <Geometry.hpp>
+#include <Laplacian.hpp>
+#include <Fem.hpp>
+using namespace pacs;
+
+// Exact solution and source.
+#include "../example/square.hpp"
+
+int main() {
+
+    // Domain and mesh.
+    Point a{0.0, 0.0};
+    Point b{1.0, 0.0};
+    Point c{1.0, 1.0};
+    Point d{0.0, 1.0};
+
+    Mesh mesh{{{a, b, c, d}}, mesh_diagram("data/square_30.poly")};
+
+    // Matrices.
+    auto [M, A, DGA] = laplacian(mesh);
+
+    // Forcing term.
+    Vector<Real> B = forcing(mesh, source);
+
+    // Numerical solution.
+    Vector<Real> numerical = solve(A, B);
+
+    // Estimates.
+    Estimator est{mesh, M, numerical, source};
+
+    // Refinement.
+    Mask mask = est.estimates > 0.75 * max(est.estimates);
+    std::vector<Polygon> diagram = mesh_refine_size(mesh, mask);
+
+}
