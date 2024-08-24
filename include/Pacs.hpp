f25e54487119261bfff57f817cd4fc871de9aa90
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-08-24 12:31:16 +0200

    Added a Pacs header

---
+++ include/Pacs.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file Pacs.hpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief 
+ * @date 2024-08-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef PACS_PACS
+#define PACS_PACS
+
+// Main headers.
+#include <Base.hpp>
+#include <Algebra.hpp>
+#include <Geometry.hpp>
+#include <Fem.hpp>
+#include <Laplacian.hpp>
+#include <Statistics.hpp>
+
+#endif
